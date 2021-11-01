4d3d6354b4ee45e719083ee371584a95136118bb
unknown <briankingDCC@gmail.com>
2021-11-01 15:26:44 -0400

    imported module from account.js, wrote functions for atm.js using account.js variables module. exported new functions made in atm.js

--- atm.js
+++ atm.js
@@ -1,25 +1,58 @@
 "use strict";
+const {pin, balance} = require("./account.js");
 //TODO: Import necessary values from account.js
-
-function getBalance() {
+console.log(pin);
+function getBalance(accountBalance) {
   //TODO: Return the customer's acct. balance
-}
+  if(validatePin() === true){
+    console.log(balance)
+  }
+  else {
+    validatePin === false
+    return
+  }
+} 
+
 
 function withdraw(withdrawAmount) {
-  //TODO: withdraw amount from current acct. balance
-  // Log the current balance after withdrawal is made
-}
+  if(validatePin() === true){            //TODO: withdraw amount from current acct. balance
+    balance -= withdrawAmount;
+    console.log(balance);
+  }
+  else {
+    return
+  }
 
 function deposit(depositAmount) {
+  if(validatePin() === true){
+    balance += depositAmount;
+    console.log(balance);
+  }
+  else {
+    return
+  }
+
   //TODO: deposit amount to current acct. balance
   // Log the current balance after deposit is made
 }
 
 function validatePin(enteredPin) {
-  //TODO: Check if entered pin matches account.js pin
-  //Allow access to ATM if matching
-  //Return value should be a Boolean (true or false)
+  if(enteredPin === pin){                //TODO: Check if entered pin matches account.js pin
+      return true                        //Allow access to ATM if matching
+  }                                      //Return value should be a Boolean (true or false)
+  else{
+    return false
+  }
+
+
   //In English: if the PIN entered is correct, return True. Otherwise, return False.
 }
 
 //TODO: Export these functions
+
+module.exports = {
+  enterPin: validatePin(),
+  checkBalance: getBalance(),
+  makeWithdraw: withdraw(),
+  makeDeposit: deposit(),
+};
