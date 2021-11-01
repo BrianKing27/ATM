0217cb9c136e96ae02d8d97e4db696b5ae613bd5
unknown <briankingDCC@gmail.com>
2021-11-01 15:25:19 -0400

    Created variables balance and pin. exported variables.

--- account.js
+++ account.js
@@ -3,5 +3,11 @@
 //! Remember to run npm init to create your Node application!
 
 //TODO: Create variables for account balance & account pin
+let accountBalance = 3750;
+let accountPin = 9731;
 
 //Export these variables
+module.exports = {
+    balance: accountBalance,
+    pin: accountPin,
+};
